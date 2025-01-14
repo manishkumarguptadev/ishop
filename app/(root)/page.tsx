c576dc0f321edfb86a1599edfe69ce031b78086a
manishkumarguptadev <manishkumarguptadev@gmail.com>
2025-01-14 22:53:01 +0530

    product listing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
-
-export default function Home() {
+const HomePage = () => {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <>
+      <ProductList title="Newest Arrivals" data={sampleData.products} />
+    </>
   );
-}
+};
+export default HomePage;
