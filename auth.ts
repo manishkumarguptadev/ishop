61df6d19d6414c9d58c0991e1616db5f3e9a5f49
manishkumarguptadev <manishkumarguptadev@gmail.com>
2025-02-23 13:00:19 +0530

    role added to session

--- auth.ts
+++ auth.ts
@@ -4,6 +4,9 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import prisma from "./prisma/client";
 
+import { getUserById } from "@/lib/data/user";
+import { Role } from "@prisma/client";
+
 export const {
   handlers: { GET, POST },
   auth,
@@ -12,6 +15,30 @@ export const {
 } = NextAuth({
   pages: {
     signIn: "/sign-in",
+  },
+  callbacks: {
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as Role;
+      }
+
+      return session;
+    },
   },
 
   adapter: PrismaAdapter(prisma),
